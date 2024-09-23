fa3a9253ee75d4445560d714e9a3ceeaa1d06178
Silvanos Eric <john.muchiri@student.moringaschool.com>
2024-09-23 18:46:30 +0300

    feat(models): add models for salon, customer, service, and appointment
    - Added `Salon` model with `id`, `name`, `location`, and `services` relationship.
    - Added `Customer` model with `id`, `name`, `services` relationship, and `appointments`
      relationship.
    - Added `Service` model with `id`, `name`, `price`, `salon_id`, `customers` relationship, and
      `appointments` relationship.
    - Added `Appointment` model with `id`, `created_at`, `customer_id`, and `service` fields.
    - Created `customers_services` association table for many-to-many relationship between `Customer`
      and `Service`.
    
    These changes introduce the necessary models for the salon application.

--- salon/models.py
+++ salon/models.py
@@ -1,4 +1,60 @@
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import (Column, Integer, String, DateTime, func, Table,
+                        ForeignKey)
+from sqlalchemy.orm import declarative_base, relationship
 
 # Create Base class, subclassed by all moddels
 Base = declarative_base()
+
+customers_services = Table(
+    'customers_services', Base.metadata,
+    Column('customer_id', ForeignKey('customers.id'), primary_key=True),
+    Column('service_id', ForeignKey('services.id'), primary_key=True))
+
+
+# Salon model
+class Salon(Base):
+    __tablename__ = 'salons'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    location = Column(String, nullable=False)
+
+    services = relationship('Service', backref='salon')
+
+
+# Cusotmer model
+class Customer(Base):
+    __tablename__ = 'customers'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(Integer, nullable=False)
+
+    services = relationship('Service',
+                            secondary=customers_services,
+                            back_populates='customers')
+    appointments = relationship('Appointment', backref='customer')
+
+
+# Service model
+class Service(Base):
+    __tablename__ = 'services'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    price = Column(Integer, nullable=False)
+    salon_id = Column(Integer, ForeignKey('salons.id'))
+
+    customers = relationship('Customer',
+                             secondary=customers_services,
+                             back_populates='services')
+    appointments = relationship('Appointment', backref='service')
+
+
+# Appointment model
+class Appointment(Base):
+    __tablename__ = 'appointments'
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column(DateTime, default=func.now())
+    customer_id = Column(Integer, ForeignKey('customers.id'))
+    service = Column(Integer, ForeignKey('services.id'))
